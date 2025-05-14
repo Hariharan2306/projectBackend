7320895335fa09693e797280929223c3371a984d
Hariharam-ciq <hariharan.j@commerceiq.ai>
2025-05-14 08:23:55 +0530

    added register id validation for users

--- src/Service/authService.ts
+++ src/Service/authService.ts
@@ -1,7 +1,10 @@
+import axios from "axios";
 import omit from "lodash/omit";
+import get from "lodash/get";
 import { getNextSequenceWithPrefix } from "../Database/daoService";
 import usersModel from "../Models/userModels";
 import type { CreateUserData, LoginUserData } from "../types/common";
+import { isEmpty } from "lodash";
 
 export const loginService = async ({
   userMail,
@@ -26,10 +29,34 @@ export const loginService = async ({
 };
 export const createUserService = async (request: CreateUserData) => {
   try {
+    if (request.reciever && isEmpty(request.registeredId))
+      throw new Error("Registered Id not found");
+    const orgId = get(request, "registeredId", "");
+
+    const charitybaseUrl = process.env.CHARITYBASE_API_URL || "";
+    const charityApiKey = process.env.CHARITYBASE_API_KEY || "";
+    const query = `{ CHC { getCharities(filters: { id: ${orgId} }) {count}}}`;
+
+    const response = await axios.post(
+      charitybaseUrl,
+      { query },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Apikey ${charityApiKey}`,
+        },
+      }
+    );
+    const count = get(response, "data.data.CHC.getCharities.count", false);
+
+    if (!count || count === 0) {
+      throw new Error(`Failed while Validating Organization Id - ${orgId}`);
+    }
+
     await usersModel.create({
       userId: await getNextSequenceWithPrefix("users_count", "USER"),
       ...omit(request, "registeredId"),
-      orgId: request.registeredId,
+      orgId,
     });
   } catch (e) {
     throw new Error(`Failed while Creating User - ${(e as Error).message}`);
